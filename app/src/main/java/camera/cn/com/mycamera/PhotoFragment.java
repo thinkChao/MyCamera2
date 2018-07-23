283a14cd65affd45759dc7fa420002f204b051dc
thinkChao <zhangchao4u@163.com>
2018-07-23 20:56:51 +0800

    can start preview

--- app/src/main/java/camera/cn/com/mycamera/PhotoFragment.java
+++ app/src/main/java/camera/cn/com/mycamera/PhotoFragment.java
@@ -11,11 +11,15 @@ import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
 import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -25,30 +29,30 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.Button;
 
 import java.util.Arrays;
 
-
 public class PhotoFragment extends Fragment {
-    String TAG = "MyLog-PhotoFragment";
+    String TAG = "MyLog-PhotoFragment.java";
     private CameraManager mCameraManager;
     private Context mContext;
     private int numCamera;
-    CameraCharacteristics frontCameraCharacteristics;
-    CameraCharacteristics backCameraCharacteristics;
-    String frontCameraId = null;
-    String backCameraId = null;
-    CameraDevice mCameraDevice;
+    private CameraCharacteristics frontCameraCharacteristics;
+    private CameraCharacteristics backCameraCharacteristics;
+    private String frontCameraId = null;
+    private String backCameraId = null;
+    private CameraDevice mCameraDevice;
     private Handler mBackgroundHandler = null;
     private HandlerThread mBackgroundThread = null;
     private PhotoTextureView mTextureView;
     private CaptureRequest.Builder mPreviewRequestBuilder;
-    private ImageReader mImageReader;
-    private Size mPreviewSize;
+    SurfaceTexture mSurfaceTexture;
+    CaptureRequest mCaptureRequest;
+    private Button mShutterButton;
 
-    public PhotoFragment() {
+    public PhotoFragment(){
+
     }
 
     @SuppressLint("ValidFragment")
@@ -65,7 +69,8 @@ public class PhotoFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mTextureView = (PhotoTextureView) view.findViewById(R.id.photo_texture_view);
-
+        mShutterButton = (Button)view.findViewById(R.id.shutter_button);
+        mShutterButton.setOnClickListener(shutterButtonCllickListener);
     }
 
 
@@ -74,14 +79,8 @@ public class PhotoFragment extends Fragment {
         super.onResume();
         startBackgroundThread();
         detectCamera();
-        //openCamera();
-        if (mTextureView.isAvailable()) {
-            openCamera();
-        } else {
-            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
-            Log.e(TAG,"执行了这一步");
-        }
-        startPreview();
+        mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        openCamera();
     }
 
     /**
@@ -91,6 +90,7 @@ public class PhotoFragment extends Fragment {
      *      3、获取每个摄像头的参数信息
      **/
     public void detectCamera() {
+        Log.e(TAG,"detectCamera()=======>>");
         mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);//得到CameraManager
         try {
             final String[] ids = mCameraManager.getCameraIdList();
@@ -117,6 +117,7 @@ public class PhotoFragment extends Fragment {
      * 第二步：打开相机
      **/
     public void openCamera() {
+        Log.e(TAG,"openCamera()=======>>");
         try {
             if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
                 ActivityCompat.requestPermissions(getActivity(),new String[]{android.Manifest.permission.CAMERA},0);
@@ -128,61 +129,69 @@ public class PhotoFragment extends Fragment {
         }
     }
 
-    /*
-    创建openCamera(backCameraId, stateCallback, mBackgroundHandler)中的第2个参数
-     */
+    /**
+     *创建openCamera(backCameraId, stateCallback, mBackgroundHandler)中的第2个参数
+     **/
     private CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(CameraDevice cameraDevice) {
-            mCameraDevice = cameraDevice;
+            Log.e(TAG,"stateCallback.onOpened()=======>>");
+            mCameraDevice = cameraDevice;//获取CameraDevice
+
         }
 
         @Override
         public void onDisconnected(CameraDevice cameraDevice) {
-            cameraDevice.close();
+            Log.e(TAG,"stateCallback.onDisconnected()=======>>");
+            cameraDevice.close();//关闭获取CameraDevice
         }
 
         @Override
         public void onError( CameraDevice cameraDevice, int i) {
+            Log.e(TAG,"stateCallback.onError()=======>>");
             cameraDevice.close();
         }
     };
 
-    /*
-    创建openCamera(backCameraId, stateCallback, mBackgroundHandler)中的第3个参数
-     */
+    /**
+     *创建openCamera(backCameraId, stateCallback, mBackgroundHandler)中的第3个参数
+     **/
     private void startBackgroundThread() {
+        Log.e(TAG,"startBackgroundThread()=======>>");
         mBackgroundThread = new HandlerThread("CameraBackground");
         mBackgroundThread.start();
         mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
     }
 
-    /*
-    第三部：开启预览
-     */
+    /**
+     *第三步：开启预览
+     **/
     public void startPreview(){
+        Log.e(TAG,"startPreview()=======>>");
+        if(mCameraDevice == null){
+            return;
+        }
         try {
-            SurfaceTexture surfaceTexture = mTextureView.getSurfaceTexture();
-            assert surfaceTexture != null;
-            if(surfaceTexture != null)
-                Log.e(TAG,"surfaceTexture不为空");
-            else
-                Log.e(TAG,"surfaceTexture为空");
-            surfaceTexture.setDefaultBufferSize(1920,1080);
-            Surface surface = new Surface(surfaceTexture);
-
+            mSurfaceTexture.setDefaultBufferSize(1920,1080);
+            Surface surface = new Surface(mSurfaceTexture);
             mPreviewRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            mPreviewRequestBuilder.addTarget(surface);
-
-            mCameraDevice.createCaptureSession(Arrays.asList(surface, mImageReader.getSurface()),
+            mPreviewRequestBuilder.addTarget(surface);//添加一个surface用来接收数据
+            mCaptureRequest = mPreviewRequestBuilder.build();
+            mCameraDevice.createCaptureSession(Arrays.asList(surface),
                     new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured( CameraCaptureSession cameraCaptureSession) {
-
+                            Log.e(TAG,"startPreview()=======>>onConfigured()");
+                            try {
+                                cameraCaptureSession.setRepeatingRequest(mCaptureRequest,captureCallback,mBackgroundHandler);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
                         }
 
                         @Override
                         public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+                            Log.e(TAG,"startPreview()=======>>onConfigureFailed()");
 
                         }
                     },null);
@@ -190,28 +199,74 @@ public class PhotoFragment extends Fragment {
             e.printStackTrace();
         }
     }
+
     private final TextureView.SurfaceTextureListener mSurfaceTextureListener
             = new TextureView.SurfaceTextureListener() {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
-            openCamera();
-            Log.e(TAG,"可以用");
+            Log.e(TAG,"onSurfaceTextureAvailable()=======>>");
+            mSurfaceTexture = surfaceTexture;
+            startPreview();
         }
 
         @Override
         public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
-            Log.e(TAG,"可以用2");
+            Log.e(TAG,"onSurfaceTextureSizeChanged()=======>>");
         }
 
         @Override
         public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+            Log.e(TAG,"onSurfaceTextureDestroyed()=======>>");
             return false;
         }
 
         @Override
         public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
-
+           // Log.e(TAG,"onSurfaceTextureUpdated()=======>>");
         }
     };
 
+    private CameraCaptureSession.CaptureCallback captureCallback = new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+            super.onCaptureStarted(session, request, timestamp, frameNumber);
+        }
+
+        @Override
+        public void onCaptureProgressed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureResult partialResult) {
+            super.onCaptureProgressed(session, request, partialResult);
+        }
+
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+        }
+
+        @Override
+        public void onCaptureFailed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureFailure failure) {
+            super.onCaptureFailed(session, request, failure);
+        }
+
+        @Override
+        public void onCaptureSequenceCompleted(@NonNull CameraCaptureSession session, int sequenceId, long frameNumber) {
+            super.onCaptureSequenceCompleted(session, sequenceId, frameNumber);
+        }
+
+        @Override
+        public void onCaptureSequenceAborted(@NonNull CameraCaptureSession session, int sequenceId) {
+            super.onCaptureSequenceAborted(session, sequenceId);
+        }
+
+        @Override
+        public void onCaptureBufferLost(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull Surface target, long frameNumber) {
+            super.onCaptureBufferLost(session, request, target, frameNumber);
+        }
+    };
+
+    View.OnClickListener shutterButtonCllickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.e(TAG,"onClick()=======>>ShutterButton Clicked");
+        }
+    };
 }
--- app/src/main/java/camera/cn/com/mycamera/PhotoTextureView.java
+++ app/src/main/java/camera/cn/com/mycamera/PhotoTextureView.java
@@ -6,8 +6,6 @@ import android.view.TextureView;
 
 public class PhotoTextureView extends TextureView {
 
-    private int mRatioWidth = 0;
-    private int mRatioHeight = 0;
 
     public PhotoTextureView(Context context) {
         super(context);
@@ -24,16 +22,16 @@ public class PhotoTextureView extends TextureView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int width = MeasureSpec.getSize(widthMeasureSpec);
-        int height = MeasureSpec.getSize(heightMeasureSpec);
-        if (0 == mRatioWidth || 0 == mRatioHeight) {
-            setMeasuredDimension(width, height);
-        } else {
-            if (width < height * mRatioWidth / mRatioHeight) {
-                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
-            } else {
-                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
-            }
-        }
+//        int width = MeasureSpec.getSize(widthMeasureSpec);
+//        int height = MeasureSpec.getSize(heightMeasureSpec);
+//        if (0 == mRatioWidth || 0 == mRatioHeight) {
+//            setMeasuredDimension(width, height);
+//        } else {
+//            if (width < height * mRatioWidth / mRatioHeight) {
+//                setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+//            } else {
+//                setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+//            }
+//        }
     }
 }
